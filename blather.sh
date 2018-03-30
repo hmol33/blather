0d2fb1f67f83c8024578173c1c73151bd24b3e72
Hans Molenaar <hmol33@gmail.com>
2018-03-30 22:09:25 +0200

    modified:   blather.sh 	modified:   config/commands.conf 	modified:   config/options.yaml

--- blather.sh
+++ blather.sh
@@ -8,14 +8,13 @@ sleep .1
 # set some shortcuts to use in the commands file
 
 #export VOICE="/usr/bin/festival --tts"
-#export VOICE="/usr/bin/espeak"
-export VOICE="/usr/bin/flite"
+export VOICE="/usr/bin/espeak"
 sleep .1
-export PLUGINS="/home/pi/blather/config/plugins"
+export PLUGINS="$HOME/blather/config/plugins"
 sleep .1
-export CONFIGDIR="/home/pi/blather/config"
+export CONFIGDIR="$HOME/blather/config"
 sleep .1
-export CLIP="/home/pi/.local/share/clipit/history"
+export CLIP="$HOME/.local/share/clipit/history"
 sleep .1
 # export KEYPRESS="xvkbd -xsendevent -secure -text"
 export KEYPRESS="xdotool key"
@@ -30,16 +29,16 @@ sleep .1
 export CLICK="xdotool click"
 # Generally, left = 1, middle = 2, right = 3, wheel up = 4, wheel down = 5
 sleep .1
-export BROWSER="firefox"
+export BROWSER="chromium-browser"
 sleep .1
-export CHBROWSER="google-chrome"
+export CHBROWSER="chromium-browser"
 sleep .1
-export EDITOR="atom"
+export EDITOR="geany"
 sleep .1
-export FM="thunar"
+export FM="pcmanfm"
 sleep .3
 
 # start blather in continuous mode with the GTK GUI
 # and a history of 20 recent commands
-bash /home/pi/blather/language_updater.sh
+/home/pi/blather/./language_updater.sh
 python2 /home/pi/blather/Blather.py
